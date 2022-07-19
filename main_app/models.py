1b881814399947d5b29306d0879287865eae65bd
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-19 16:08:41 -0400

    Database is connected + admin login works

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Guitar(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.brand
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render
-
-# Add the Cat class & list and view function below the imports
-class Guitar:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, brand, model):
-    self.name = name
-    self.brand = brand
-    self.model = model
-
-guitars = [
-  Guitar("'60s Electric Guitar - Bourbon Burst", 'Gibson', 'Les Paul Standard')
-]
+from .models import Guitar
 
 # View functions
 def home(request):
@@ -19,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def guitars_index(request):
-    return render(request, 'guitars/index.html', { 'guitars' : guitars})
+  guitars = Guitar.objects.all()
+  return render(request, 'guitars/index.html', { 'guitars' : guitars})
