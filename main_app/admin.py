d03be005fb1dc7f2a519cc34ed88523e99805f93
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-21 14:19:18 -0400

    TunedTo model is added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 # import your models here
-from .models import Guitar
+from .models import Guitar, TunedTo
 # Register your models here.
 admin.site.register(Guitar)
+admin.site.register(TunedTo)
--- main_app/models.py
+++ main_app/models.py
@@ -12,3 +12,13 @@ class Guitar(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'guitar_id': self.id})
+
+class TunedTo(models.Model):
+    date = models.DateField()
+    tuning = models.CharField(max_length=8)
+
+    guitar = models.ForeignKey(Guitar, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # return f"{self.get_tuning_display()} on {self.date}"
+        return self.tuning
