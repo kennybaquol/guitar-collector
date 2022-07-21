3df7a7d7ec4783dfa3031cacbdc32a97425654f4
kennybaquolmusic@gmail.com <kennybaquolmusic@gmail.com>
2022-07-21 13:53:38 -0400

    Part 3 is complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Guitar
 
 # View functions
@@ -20,4 +20,12 @@ def guitars_detail(request, guitar_id):
 class GuitarCreate(CreateView):
   model = Guitar
   fields = '__all__'
+  success_url = '/guitars/'
+
+class GuitarUpdate(UpdateView):
+  model = Guitar
+  fields = '__all__'
+
+class GuitarDelete(DeleteView):
+  model = Guitar
   success_url = '/guitars/'
